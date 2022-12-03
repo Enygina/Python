1a7697b111bd4a9505bf9e6eeae9996cb7a9e879
Olga Enygina <olga.enygina@bk.ru>
2022-12-03 13:47:53 +0300

    add task decision

--- HomeWork/sem3task2.py
+++ HomeWork/sem3task2.py
@@ -5,7 +5,7 @@ import math
 
 
 def get_numbers():  # список чисел от пользователя
-    num_list = [int(i) for i in input('Введите числа через запятую').split(",")]
+    num_list = [int(i) for i in input('Введите числа через запятую ').split(",")]
     return num_list
 
 
@@ -13,19 +13,22 @@ def multiplication_pair(lst):  # перемножение пар через их
     mult_pair=[]
     while len(lst) >1:
         mult_pair.append(lst[0]*lst[-1])
+        print(f'{lst[0]} * {lst[- 1]}')
         del lst[0]
         del lst[-1]
+    if len(lst) == 1:
+        mult_pair.append(lst[0] ** 2)
+        print(f'{lst[0]} * {lst[- 1]}')
     return mult_pair
 
 def mult_pair(lst): # перемножение пар путем сторого деления длинны списка
     res = []
-    for i in range(len(lst) // 2):
+    for i in range(math.ceil(len(lst) / 2)):
         res.append(lst[i] * lst[-i - 1])
-        print({lst[i]}, {lst[-i - 1]})
+        print(f'{lst[i]} * {lst[-i - 1]}')
         i += 1
-
     return res
 
 
-print(f'Произведение пар чисел: {mult_pair(get_numbers())}')
-# print(f'Произведение пар чисел: {multiplication_pair(get_numbers())}')
+# print(f'Произведение пар чисел: {mult_pair(get_numbers())}')
+print(f'Произведение пар чисел: {multiplication_pair(get_numbers())}')
