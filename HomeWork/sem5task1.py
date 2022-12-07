b240c07e93f9a84403b6589c344fd983a1366f14
Olga Enygina <olga.enygina@bk.ru>
2022-12-07 20:29:44 +0300

    add task decision

--- HomeWork/sem5task1.py
+++ HomeWork/sem5task1.py
@@ -1,11 +1,61 @@
-# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+# Создайте программу для игры с конфетами человек против человека.
+import math
+
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая
+# ход друг после друга. Первый ход определяется жеребьёвкой. За один
+# ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему
+# последний ход. Сколько конфет нужно взять первому  игроку, чтобы забрать
+# все конфеты у своего конкурента
+# a) Добавьте игру против бота
+# b) Подумайте как наделить бота ""интеллектом""
+
+n = int(input('Выбери сколько конфет я у тебя выиграю '))
+max_step = int(input('Теперь выбери, сколько максимум можно забрать за один ход '))
+human = int(input('Рискни сделать первый ход: '))
+rem_div = n % (max_step + 1)
+ai = 0
+# проверка введенного числа
+if human > max_step or human < 1:
+    human = int(input(f'Кто-то забыл правила игры...поробуй еще раз '))
+
+# первый шаг
+def first_step(human, max_step, n, rem_div):  # первый ход:
+    if human in range(0, max_step + 1):
+        if human > rem_div:
+            ai = (rem_div + (max_step + 1)) - human
+            n = n - ai - human
+        elif human < rem_div:
+            ai = rem_div - human
+            n = n - ai - human
+        elif human == rem_div:
+            print('Сдаюсь, давай еще ')
+            exit()
+        print(f'Мой ход {ai}, осталось конфет:  {n} ')
+    return n
+
+#
+def second_to_last_step(n):
+    while n > max_step + 1:
+        human = int(input('Твой ход: '))
+        if human > max_step or human < 1:
+            human = int(input(f'Кто-то забыл правила игры...поробуй еще раз'))
+        ai = max_step + 1 - human
+        print(f'Мой ход {ai}, осталось конфет {n - human - ai} ')
+        n = n - human - ai
+    return n
 
 
-text = input('Введите текст:')
-text = text.split(' ')
-del_t = 'абв'
-new_text = []
-for word in text:
-    if del_t not in word:
-        new_text.append(word)
-print(' '.join(str(i) for i in new_text))
+def last_step(n):
+    human = int(input('Твой ход: '))
+    if human > max_step or human < 1:
+        human = int(input(f'Кто-то забыл правила игры...поробуй еще раз'))
+    if n-human == 0:
+        print('Поздравляю! Бери свои конфетки, я требую реванш!')
+    else:
+        print(f'Мой ход {n-human}')
+        print(f'Гони свои конфетки! Готов отиграться? ')
+
+
+
+
+last_step(second_to_last_step(first_step(human, max_step, n, rem_div)))
