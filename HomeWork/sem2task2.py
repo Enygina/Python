d6229df2e1a949239a97963b52a588806c37d014
Olga Enygina <olga.enygina@bk.ru>
2022-12-12 19:44:53 +0300

    add optimisation

--- HomeWork/sem2task2.py
+++ HomeWork/sem2task2.py
@@ -2,15 +2,28 @@
 # N и выдает набор произведений чисел от 1 до N.
 import math
 
-n = int(input('Введите N: '))
-a = 1
+# n = int(input('Введите N: '))
+# a = 1
 # f = 1
 # for i in range(n):
 #     print(f, end=' ')
 #     f *= a + 1
 #     a += 1
+#
+# for i in range(n):
+#     f = math.factorial(a)
+#     print(f, end=' ')
+#     a += 1
 
-for i in range(n):
-    f = math.factorial(a)
-    print(f, end=' ')
-    a += 1
+
+from itertools import accumulate
+import operator
+
+N = int(input('Введите число: '))
+
+
+def get_prods(N):
+    return list(accumulate([x for x in range(1, N + 1)], operator.mul))
+
+
+print(get_prods(N))
