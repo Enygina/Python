f9761f3f75e37bbe8f8f6b7efd7560d30ec82edb
Olga Enygina <olga.enygina@bk.ru>
2022-12-12 17:57:18 +0300

    add optimisation

--- HomeWork/sem3task1.py
+++ HomeWork/sem3task1.py
@@ -2,16 +2,21 @@
 # Напишите программу, которая найдёт сумму
 # элементов списка, стоящих на нечётной позиции.
 
-def get_numbers(): # список чисел от пользователя
+def get_numbers():  # список чисел от пользователя
     num_list = [int(i) for i in input('Введите числа через запятую ').split(",")]
     return num_list
 
-def find_odd_pos(list): # нахождение нечетных позиций
-    odd_pos = []
-    for i in range (len(list)):
-        i=1
-        odd_pos.append(list[i])
-        i+=2
-    return odd_pos
+#
+# def find_odd_pos(list):  # нахождение нечетных позиций
+#     odd_pos = []
+#     for i in range(len(list)):
+#         i = 1
+#         odd_pos.append(list[i])
+#         i += 2
+#     return odd_pos
+# print(f"Сумма чисел с нечетным индексом: {sum(find_odd_pos(get_numbers()))}")
 
-print(f"Сумма чисел с нечетным индексом: {sum(find_odd_pos(get_numbers()))}")
+print(f'Сумма чисел с нечетным индексом: {sum(get_numbers()[1::2])}')
+
+
+
