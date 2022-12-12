416e568555fb8e9b0de2cfe566a0abfbc181d1c3
Olga Enygina <olga.enygina@bk.ru>
2022-12-12 19:34:56 +0300

    add optimisation

--- HomeWork/sem3task4.py
+++ HomeWork/sem3task4.py
@@ -4,16 +4,21 @@
 dcl = int(input('Введите число : '))
 
 
-def decimal_to_binary(dcl):
-    bnr=[]
-    while dcl > 0:
-        if dcl%2 ==0:
-            bnr.append(0)
-            dcl/=2
-        else:
-            dcl%2 !=0
-            bnr.append(1)
-            dcl=(dcl-1)/2
-    return bnr
+# def decimal_to_binary(dcl):
+#     bnr=[]
+#     while dcl > 0:
+#         if dcl%2 ==0:
+#             bnr.append(0)
+#             dcl/=2
+#         else:
+#             dcl%2 !=0
+#             bnr.append(1)
+#             dcl=(dcl-1)/2
+#     return bnr
+#
+# print(decimal_to_binary(dcl))
 
-print(decimal_to_binary(dcl))
+
+def dec_to_bi(d):
+    return bin(d).replace("0b","")
+print(dec_to_bi(dcl))
